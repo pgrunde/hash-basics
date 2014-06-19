68d1a65b88fcc0e4f69af07233ace3ce310a11e1
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-19 14:02:10 -0600

    tournament wins added

--- 07_tournament_wins.rb
+++ 07_tournament_wins.rb
@@ -3,8 +3,7 @@ require_relative "person"
 # Add Bjorn's tournament wins below to the BJORN_BORG hash. Don't edit the perosn.rb file!
 # After you add the tournament data, print out the first and last year that Bjorn Borg won Wimbledon.
 
-wins = {
-  tournament_wins: {
-    wimbledon: ["1976", "1977", "1978", "1979", "1980"]
-  }
-}
+wins = { tournament_wins: { wimbledon: ["1976", "1977", "1978", "1979", "1980"] } }
+BJORN_BORG["wins"] = wins
+p BJORN_BORG["wins"][:"tournament_wins"][:"wimbledon"][0]
+p BJORN_BORG["wins"][:"tournament_wins"][:"wimbledon"][4]

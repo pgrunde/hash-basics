53c42fc2d7c0351eec3dc857fbb75df0063d390d
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-19 13:54:56 -0600

    added print address

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,9 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+# BJORN_BORG["address"].each_key do |item|
+#   print BJORN_BORG["address"][item].to_s + ", "
+# end
+# puts
+
+p "#{BJORN_BORG["address"]["street"]}, #{BJORN_BORG["address"]["city"]}, #{BJORN_BORG["address"]["state"]}, #{BJORN_BORG["address"]["zip_code"]}"

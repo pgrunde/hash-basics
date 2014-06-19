550d062d997da3f2d006ca2030ed1cc4527ea3f4
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-19 13:56:39 -0600

    added favorite food

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,4 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+p "Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]}, #{BJORN_BORG["favorite_foods"][2]}."
